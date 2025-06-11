47fd8ecca3b1903fada7528e4756500d517c740f
Meghna Varma <114867153+meghnavarma25@users.noreply.github.com>
2025-06-11 07:50:13 +0530

    Update proxy.js

--- src/pages/api/proxy.js
+++ src/pages/api/proxy.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   try {
-    const backendRes = await fetch("http://13.126.126.215/classify", {
+    const backendRes = await fetch("http://13.204.62.156/classify", {
       method: req.method,
       headers: {
         "Content-Type": "application/json",
